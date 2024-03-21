55efb5c5a1cfa40b31e42c94f65beb6a20303b2f
Sanjeevni Srivastava <aec.cse.sanjeevnisrivastava@gmail.com>
2024-03-21 21:15:54 +0530

    Add files via upload

--- append.java
+++ append.java
@@ -1,7 +1,9 @@
-class A{
-public static void main(String args[]){
-StringBuffer sb=new StringBuffer("Hello");
-sb.insert(1,"java");
+class A
+{
+public static void main(String args[])
+{
+StringBuffer sb=new StringBuffer("sanjeevni");
+sb.reverse();
 System.out.println(sb);
 }
 }
